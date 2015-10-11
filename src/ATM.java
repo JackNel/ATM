1725bd2d9aea22dbbefaecdf4bedd808bd5e243e
JackNel <jcneligan@gmail.com>
2015-10-11 10:52:59 -0400

    Added create account and delete account option

--- src/ATM.java
+++ src/ATM.java
@@ -1,44 +1,76 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * Created by Jack on 10/8/15.
  */
 public class ATM {
+
+
     public static void run() throws Exception {
+
+        HashMap<String, Double> accounts = new HashMap();
+
         System.out.println("Welcome to the ATM.");
         System.out.println("What is your name?");
+
+
 
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         if (name.length() == 0) {
             throw new Exception("Not a valid name.");
         } else {
-            System.out.println("Hello " + name + ", what would you like to do today?");
+            if (!accounts.containsKey(name)) {
+                System.out.println("Hello " + name + ", Would you like to create a new account? [y/n]");
+                String create = scanner.nextLine();
+                if (create.equals ("y")) {
+                accounts.put(name, 100.00);
+                System.out.println("New Account created, your current balance is $100.00");
+                } else if (create.equals ("n")) {
+                  System.out.println("Thank you, have a good day!");
+                  System.exit(0);
+                }
+            }
         }
+
+        System.out.println("Hello " + name + ",  What would you like to do today?");
+
+        double balance = accounts.get(name);
         System.out.println("[1] Check my balance");
         System.out.println("[2] Withdraw Funds");
         System.out.println("[3] Cancel");
+        System.out.println("[4] Delete Bank Account");
         String choice = scanner.nextLine();
         int choiceNum = Integer.valueOf(choice);
         if (choiceNum == 1) {
-            System.out.println("Your balance is $100");
+            System.out.println("Your balance is $" + balance);
 
         } else if (choiceNum == 3) {
             System.out.println("Thank you and please come again.");
+            System.exit(0);
 
         } else if (choiceNum == 2) {
             System.out.println("How much money would you like to withdraw?");
             String amount = scanner.nextLine();
             int amountNum = Integer.valueOf(amount);
-            if (amountNum > 100) {
-                throw new Exception("ATM currently only has $100.");
-            } else if (amountNum <= 100) {
-                    System.out.println("Your balance is now $" + (100 - amountNum));
-                    System.out.println("Please take your money.");
-                }
+            if (amountNum > balance) {
+                throw new Exception("ATM currently only has $" + balance);
+            } else if (amountNum <= balance) {
+                System.out.println("Your balance is now $" + (balance - amountNum));
+                System.out.println("Please take your money.");
+            }
 
+        } else if (choiceNum == 4) {
+            System.out.println("Please enter the name of your account.");
+            String n = scanner.nextLine();
+            if (accounts.containsKey(n)) {
+                accounts.remove(n);
+                System.out.println("Account name: " + n + " has been deleted");
+            }
+            else {
+                System.out.println(n + " is not an existing account.");
+            }
         }
-
-
     }
 }
